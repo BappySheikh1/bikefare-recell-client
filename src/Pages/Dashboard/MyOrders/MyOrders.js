57f2f4f6127aa1bad0e68048e6e2be4520e6f416
Your Name Main <you@example.com>
2022-11-24 08:08:13 -0800

    my orders query added

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useContext } from 'react';
+import {AuthContext} from '../../../contexts/AuthProvider'
 
 const MyOrders = () => {
+    const {user}=useContext(AuthContext)
     const {data:bookedItems = [],isLoading,refetch}=useQuery({
-        queryKey:['bookedItem'],
+        queryKey:['bookedItem',user?.email],
         queryFn: async ()=>{
-          const res = await fetch(`http://localhost:4000/bookedItem`)
+          const res = await fetch(`http://localhost:4000/bookedItem?email=${user?.email}`)
           const data=await res.json()
           return data
         }
@@ -24,19 +26,31 @@ const MyOrders = () => {
     <thead>
       <tr>
         <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
+        <th>image</th>
+        <th>Title</th>
+        <th>price</th>
+        <th>action</th>
       </tr>
     </thead>
     <tbody>
      
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
+      {
+        bookedItems.map((bookedItem ,i)=> 
+        <tr key={bookedItem._id}>
+        <th>{i + 1}</th>
+        <td>
+        <div className="avatar">
+          <div style={{width :'60px'}} className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+            <img src={user.photoURL}  alt=''/>
+          </div>
+        </div>
+        </td>
+        <td>{bookedItem.productName}</td>
+        <td>${bookedItem.price}</td>
+        <td><button className='btn btn-xs rounded-xl bg-blue-600 hover:bg-blue-600 border-none'>Pay</button></td>
       </tr>
+      )
+      }
    
     </tbody>
   </table>
