0a460aa89f2a44ce7859f8962e688d225006a9fb
Your Name Main <you@example.com>
2022-11-24 07:05:43 -0800

    my orders page added

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,9 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const MyOrders = () => {
+    const {data:bookedItems = [],isLoading,refetch}=useQuery({
+        queryKey:['bookedItem'],
+        queryFn: async ()=>{
+          const res = await fetch(`http://localhost:4000/bookedItem`)
+          const data=await res.json()
+          return data
+        }
+    })
+    if(isLoading){
+        return <div className='text-center my-9'>
+            <button className="btn btn-square loading"></button>
+        </div>
+    }
     return (
         <div>
-            <h4 className="text-3xl">My Orders Page</h4>
+            <h4 className="text-3xl">My Orders Page {bookedItems.length}</h4>
+            <div className="overflow-x-auto">
+  <table className="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+     
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+   
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
